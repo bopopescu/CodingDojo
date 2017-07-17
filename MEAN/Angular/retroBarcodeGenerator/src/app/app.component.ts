28fdea1ddaaa55fb5a3d86515cc676105dcc348d
Sushma <sushma.m812@gmail.com>
2017-07-17 15:46:29 -0700

    modified retroBarcodeGenerator

--- MEAN/Angular/retroBarcodeGenerator/src/app/app.component.ts
+++ MEAN/Angular/retroBarcodeGenerator/src/app/app.component.ts
@@ -6,15 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Retro Barcode Generator';
-  arrayofColorsHEX = [
-    "#e6ffff", "#ccffff", "#b3ffff",
-    "#66ffff", "#33ffff", "#00e6e6",
-    "#00b3b3", "#009999", "#008080",
-    "#006666", "#004d4d"
-  ]
 
-  getthatcolor = function(){
-    return (Math.floor(Math.random()*10));
-  }
 }
--- MEAN/Angular/retroBarcodeGenerator/src/app/color/color.component.ts
+++ MEAN/Angular/retroBarcodeGenerator/src/app/color/color.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./color.component.css']
 })
 export class ColorComponent implements OnInit {
+  title = 'Retro Barcode Generator';
+  arrayofColorsHEX = [
+    "#e6ffff", "#ccffff", "#b3ffff",
+    "#66ffff", "#33ffff", "#00e6e6",
+    "#00b3b3", "#009999", "#008080",
+    "#006666", "#004d4d"
+  ]
 
-  constructor() { }
+  getthatcolor = function(){
+    return (Math.floor(Math.random()*10));
+  }
+
+  constructor() {
+    
+   }
 
   ngOnInit() {
+
   }
+      
 
 }
